e39bf266924af5bdcef904c9e8af1d8995e42ed3
rlatprnr <rlatprnr@gmail.com>
2022-01-13 14:29:34 -0600

    add mouse events

--- slider/slider.js
+++ slider/slider.js
@@ -1,5 +1,6 @@
 class Slider {
   currentIndex = 0;
+  moveX = 0;
 
   constructor(selector) {
     // get slider and items
@@ -16,7 +17,51 @@ class Slider {
     const observer = new ResizeObserver(() => this.updateSlider);
     observer.observe(this.slider);
 
+    // mouse events
+    this.slider.addEventListener('mousedown', (event) => {
+      this.startDrag(event);
+    });
+    this.slider.addEventListener('mousemove', (event) => {
+      this.dragging(event);
+    });
+    this.slider.addEventListener('mouseout', () => {
+      this.endDrag(false);
+    });
+    this.slider.addEventListener('mouseup', () => {
+      this.endDrag(true);
+    });
+
     this.updateSlider();
+  }
+
+  startDrag(touch) {
+    if (!this.touch0) {
+      this.touch0 = touch;
+    }
+  }
+
+  dragging(touch) {
+    if (this.touch0) {
+      this.moveX = touch.clientX - this.touch0.clientX;
+      this.updateSlider();
+    }
+  }
+
+  endDrag(isTouchUp) {
+    if (this.touch0) {
+      this.touch0 = null;
+
+      const dx = isTouchUp ? this.slider.clientWidth / 2 : 0;
+
+      if (this.moveX < -dx && this.currentIndex < this.items.length - 1) {
+        this.currentIndex++;
+      } else if (this.moveX > dx && this.currentIndex > 0) {
+        this.currentIndex--;
+      }
+
+      this.moveX = 0;
+      this.updateSlider();
+    }
   }
 
   /**
@@ -24,7 +69,7 @@ class Slider {
    */
   updateSlider() {
     this.items.forEach((page, i) => {
-      const x = (i - this.currentIndex) * this.slider.clientWidth;
+      const x = (i - this.currentIndex) * this.slider.clientWidth + this.moveX;
       page.style.transform = `translateX(${x}px)`;
     });
   }
