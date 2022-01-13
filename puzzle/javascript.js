b044961f6c12252b0d988126fffb9ea21e174265
rlatprnr <rlatprnr@gmail.com>
2022-01-13 11:58:03 -0600

    add click event and move

--- puzzle/javascript.js
+++ puzzle/javascript.js
@@ -12,7 +12,20 @@ function setPos(el, x, y, isTile) {
   }
 }
 
+/**
+ * @param el: html element
+ * @returns [x, y]: position x, y
+ */
+function getPos(el) {
+  const [x, y] = el.getAttribute('data-pos').split(',');
+  return [parseInt(x), parseInt(y)];
+}
+
 window.onload = function () {
+  // save empty pos
+  const box = document.querySelector(`.puzzle`);
+  setPos(box, 2, 2);
+
   // get tile elements
   const tiles = Array.from(document.querySelectorAll(`.puzzle .tile`));
 
@@ -21,9 +34,17 @@ window.onload = function () {
     const x = Math.floor(i % 3);
     const y = Math.floor(i / 3);
     setPos(tile, x, y, true);
-  });
 
-  // save empty pos
-  const box = document.querySelector(`.puzzle`);
-  setPos(box, 2, 2);
+    // add click event
+    tile.addEventListener('click', () => {
+      const [tileX, tileY] = getPos(tile);
+      const [emptyX, emptyY] = getPos(box);
+
+      // check movement possibility
+      if (Math.abs(tileX - emptyX) + Math.abs(tileY - emptyY) === 1) {
+        setPos(tile, emptyX, emptyY, true);
+        setPos(box, tileX, tileY);
+      }
+    });
+  });
 };
